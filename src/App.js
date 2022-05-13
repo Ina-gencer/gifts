6dcd5577b2d84c36fb35ba35fbf2c4caa40c67c8
Пользователь <ina_gencer@mail.ru>
2022-05-14 02:07:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from 'react';
 
 function App() {
+  const [gifts, setGifts] = useState(data);
+  // console.log(data)
+  const removeItem =(id)=> {
+// console.log(id)
+let newArray = gifts.filter(element => element.id !== id);
+console.log(newArray)
+setGifts(newArray)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <div className='container'>
+        <h1>List of {gifts.length} gifts</h1>
+     </div>
+      {gifts.map((item => {
+        const{id, gifty, image} = item;
+        return(
+          <div key={id}>
+
+            <div className='container'>
+              <h2>{id} - {gifty}</h2>
+            </div>
+
+            <div className='container'>
+              <img src={image} alt="gifts" width="300px" />
+            </div>
+            <div className='container'>
+              <button onClick={()=> removeItem(id)}> Remove</button>
+            </div>
+
+          </div>
+        )
+      }))}
+      <div className='container'>
+        <button onClick={ ()=>setGifts([]) } >DELETE ALL</button>
+      </div>
+   </div>
   );
 }
 
